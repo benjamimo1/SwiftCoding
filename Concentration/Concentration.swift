64ebf17058b5d482612be9b805201bffcd09d69e
benjamimo1 <benjamimo1@MacBook-Space-Gray.local>
2018-07-31 12:19:59 -0400

    Se adiciona mas numero de botones

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,13 +12,28 @@ class Concentration {
     
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?  //optional pues podria no haber una carta boca arriba! Tendria valor "not set"
+    
     func chooseCard(at index: Int)
     {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        }
-        else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier{
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            }
+            else {
+                //either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     
